ef78c6bc14d46b0eb5b0b080fbf5c8b7b510cab0
andigela <82514593+andigela@users.noreply.github.com>
2022-02-27 12:40:52 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,102 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, TouchableOpacity, TextInput, View, Keyboard } from 'react-native';
+import Task from './components/Task';
+
+export default function App() {
+    const [input, setInput] = useState(); // qka shkruhet prej inputit momentalisht ruhet ne input
+    const [allItems, setallItems] = useState([]);   
+    function saveItems() {
+        if (input) {
+            setallItems([...allItems, input]);
+            setInput(null);
+            Keyboard.dismiss(); //key={index} onPress={() => deleteItems(index)}
+        } else {
+            ;
+        }
+        
+    }
+    function deleteItems(indexP) {
+        let deletedItems = [...allItems]; // pe ruj ni list me krejt items qka jon hi
+        deletedItems.splice(indexP, 1); // pe heki ka nje item me indexin e veqant
+        setallItems(deletedItems); // ruj edhe qato qe i kom rujt krejt edhe qato qe jon fshi
+    }
+    return (
+        <View style={styles.container}>
+            <View style={styles.taskWrapper}>
+                <Text style={styles.taskTitle}>My Tasks</Text>
+                <View style={styles.items}>   
+                    {
+                        allItems.map((item,index) => {
+                            return (
+                                <Task text={item} key={index} press={() => deleteItems(index)} />
+                            );
+                        })
+                    }
+                </View>
+            </View>
+
+            <KeyboardAvoidingView
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                style={styles.addView}
+            >
+                <TextInput style={styles.input} placeholder="Add a Task" value={input} onChangeText={text => setInput(text)} />
+                <View style={styles.addTask}><TouchableOpacity onPress={saveItems}><Text style={styles.addTaskText}>+</Text></TouchableOpacity></View>
+                
+
+                
+            </KeyboardAvoidingView>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ff6363',
+    },
+    taskWrapper: {
+        paddingTop: 80,
+        paddingHorizontal: 30,
+    },
+    taskTitle: {
+        fontWeight: 'bold',
+        fontSize: 17,
+        marginBottom: 30,
+    },
+    addView: {
+        position: 'absolute',
+        bottom: 100,
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-around',
+        
+    },
+    input: {
+        paddingVertical: 25,
+        paddingHorizontal: 55,
+        backgroundColor: '#f0f0f0',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 6,
+        maxWidth: '50%',
+        minWidth: '50%',
+        borderColor: '#000000',
+        borderWidth: 0.5,
+    },
+    addTask: {
+        backgroundColor: '#87bd7e',
+        paddingVertical: 20,
+        paddingHorizontal: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 6,
+        borderColor: '#000000',
+        borderWidth: 0.5,
+        
+        
+    },
+    addTaskText: {
+        fontSize: 20,
+    },
+});
---
+++ Task.js
@@ -0,0 +1,65 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function Task(props) {
+    return (
+        <View style={styles.items}>
+            <View style={styles.leftItems}>
+                <View style={styles.square}></View>
+                <Text style={styles.itemText}>{props.text}</Text>
+            </View>
+            <View style={styles.rightItems}>
+                <TouchableOpacity onPress={props.press}>
+                    <Text style={styles.D}>D</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    items: {
+        padding: 20,
+        backgroundColor: '#FFF',
+        marginBottom: 20,
+        borderRadius: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+
+    },
+    leftItems: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'center'
+    },
+    square: {
+        borderBottomColor: '#fa027a',
+        borderTopColor: '#ff2990',
+        borderLeftColor: '#ff2990',
+        borderRightColor: '#fa027a',
+        width: 15,
+        height: 15,
+        borderWidth: 4,
+        borderRadius: 3,
+        marginRight: 20,
+        
+    },
+    itemText: {
+        maxWidth: '80%',
+    },
+    rightItems: {
+        backgroundColor: '#ff3668',
+        width: 26,
+        height: 26,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 3,
+        
+    },
+    D: {
+        fontWeight: 'bold',
+        color: '#f7f7f7'
+    }
+
+});
